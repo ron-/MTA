1cb2bbfb47fc6065f2a3eed5ee6e03b7c8b7deca
Ron Jones <rrrron@gmail.com>
2013-01-30 19:27:34 -0500

    push

--- main.rb
+++ main.rb
@@ -2,18 +2,25 @@
 require "pry-debugger"
 
 
+
+def calcStops
+
+end
+
 puts "Train Line 'N', '6', or 'L'? "
 userTrain = gets.chomp.upcase.to_s
 
 case userTrain
 when "N"
+  userTrain = :n
   puts "Available stations: "
   puts "ts, 34, 28n, 23n, us, 8n"
 when "6"
-  userTrain = "t6"
+  userTrain = :t6
   puts "Available stations: "
   puts "gc, 33, 28_6, 23_6, us, ap"
 when "L"
+  userTrain = :l
   puts "Available stations"
   puts "8l, 6, us, 3, 1"
 else
@@ -27,9 +34,20 @@ get_on = gets.chomp.downcase.to_s
 puts "Exit station?"
 get_off = gets.chomp.downcase.to_s
 
-puts userTrain
-puts get_on
-puts get_off
+subway[:n].each do |i|
+  if get_on == i
+    index1 = i.index
+
+num_stops
+
+puts "You will travel #{num_stops} stops"
+
+
+
+
+# puts userTrain
+# puts get_on
+# puts get_off
 
 
 
